beb4f89ee60dad1a993c8fc287effcbda02ba059
CelestinaT <celestina.temilade@outlook.com>
2023-10-05 05:16:43 +0000

    ES6_data_manipulation_tasks

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const dv = new DataView(buffer);
-
-  // check if position outside range
-  if (position >= length) throw new Error('Position outside range');
-  // set the new value in the desired position
-  dv.setInt8(position, value);
-
-  return dv;
+  if (position >= length) {
+    throw new RangeError('Position outside range');
+  }
+  const buffer = new DataView(new ArrayBuffer(length));
+  buffer.setInt8(position, value);
+  return buffer;
 }
